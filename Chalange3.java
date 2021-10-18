200ebbfc5f0f4c16b0db02b5a9ce939ae15161b8
unknown <you@example.com>
2021-10-18 18:45:47 +0700

    first commit

---
+++ Chalange3.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class Chalange3 {
+    public static void main(String[] args) {
+        int nilai ;
+        Scanner skor = new Scanner(System.in);
+        System.out.print("Masukan Nilai anda");
+        nilai = skor.nextInt();
+        if(nilai >= 0 && nilai <= 25){
+            System.out.println("Nilai Anda E");
+        }else if (nilai >= 26 && nilai<=50){
+            System.out.println("Nilai Anda D");
+        }else if (nilai >= 51 && nilai<=75){
+            System.out.println("Nilai Anda C");
+        }else if (nilai>=76 && nilai<=90){
+            System.out.println("Nilai Anda B");
+        }else if (nilai>=91 && nilai<=100){
+            System.out.println("Nilai Anda A");
+        }else if (nilai>=100){
+            System.out.println("Nilai Salah");
+        }
+        
+    }
+}
